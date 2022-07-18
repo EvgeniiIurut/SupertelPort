7c02e06a02cbf36e66ea8b2403284ad893b9041b
Evgenii <john.yurut@gmail.com>
2022-07-19 00:27:51 +0300

    Fixed common names of variables

--- src/test/java/PortTest.java
+++ src/test/java/PortTest.java
@@ -4,13 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PortTest {
     final String[] strings = new String[]{"1-2", "3-4", "5-6"};
-    final String[] wrongStrings = new String[]{"1- 2", "3-4", "5-6wrong"};
     final Port port = new Port(strings);
-    final int[][] result1 = new int[][]{{1, 2}, {3, 4}, {5, 6}};
-    final int[][] result2 = new int[][]{{1, 3, 5}, {1, 3, 6}, {1, 4, 5}, {1, 4, 6}, {2, 3, 5}, {2, 3, 6}, {2, 4, 5}, {2, 4, 6}};
 
     @Test
     void should_throw_exception_for_invalid_input_indexes_data() {
+        final String[] wrongStrings = new String[]{"1- 2", "3-4", "5-6wrong"};
         var exception = assertThrows(IllegalArgumentException.class,
                 () -> new Port(wrongStrings));
         assertEquals(exception.getMessage(), "Invalid indexes. Please write correct data");
@@ -18,11 +16,14 @@ public class PortTest {
 
     @Test
     void should_return_indexes_array() {
-        assertArrayEquals(port.getIndexes(), result1);
+        final int[][] expected = new int[][]{{1, 2}, {3, 4}, {5, 6}};
+        assertArrayEquals(expected, port.getProcessedIndexes());
     }
+
 
     @Test
     void should_return_non_repeating_sequences_array_by_indexes() {
-        assertArrayEquals(port.getCombineSequences(), result2);
+        final int[][] expected = new int[][]{{1, 3, 5}, {1, 3, 6}, {1, 4, 5}, {1, 4, 6}, {2, 3, 5}, {2, 3, 6}, {2, 4, 5}, {2, 4, 6}};
+        assertArrayEquals(expected, port.getCombineSequences());
     }
 }
